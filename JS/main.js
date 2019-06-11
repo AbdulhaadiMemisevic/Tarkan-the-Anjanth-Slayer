c646092a1bbdc824e9d86fedee876f5dbaba256b
anjanathaadi <47528875+anjanathaadi@users.noreply.github.com>
2019-06-11 19:54:51 -0400

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -51,3 +51,72 @@ decrypted_content.innerHTML = connect_content;
 
 
 }
+
+
+
+
+
+
+
+
+var guesstext = document.getElementById("guess_text");
+
+
+var randomNum = Math.floor((Math.random() * 100) + 1);
+
+function numGuess(){
+
+	var guess = document.getElementById("guesses").value;
+
+	
+
+	var chances = 0
+
+
+	if (guess == random_Num) {
+			guess_text.innerHTML = ("Congratulations! You got me in " + (guesses) +" guesses")
+			
+		}
+
+
+
+		else if (guess > random_Num){
+			guesses++;
+			guess_text.innerHTML = ("Guess is too high");
+			
+		}
+		else if (guess < random_Num){
+			guesses++;
+			guess_text.innerHTML = ("Guess is too low");
+
+		}
+}
+
+
+
+var guess = document.getElementById("Guess");
+	
+
+	
+
+	var guesses = 0;
+
+	var random_Num = Math.floor((Math.random() * 1000) + 1);
+	
+
+
+
+
+function generateNum(){
+
+
+
+var guess_text = document.getElementById("guesstext");
+
+
+
+
+
+
+}	
+
